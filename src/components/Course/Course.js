c4d00ec2e302760719d6648b731a170167ee3732
nabilAhmedN <nabilahmednahid@gmail.com>
2022-10-26 03:02:44 +0600

    toggle button added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Course = ({course}) => {
     const { title, photo, subscribe, duration, titleName } = course;
 
     return (
-        <div className="col-lg-4 col-md-12 col-sm-12">
-            <Card>
-            <Card.Img variant="top" src={photo} />
-            <Card.Body>
-                <div className="d-flex justify-content-between pb-2">
-                <span>{subscribe} students</span>
-                <span>{duration}m</span>
-                </div>
-                <Card.Title className="fw-bolder">{title}</Card.Title>
-                <Card.Text>{titleName}</Card.Text>
-                <Button variant="primary">Features</Button>
-            </Card.Body>
-            </Card>
-        </div>
+      <div className="col-lg-4 col-md-12 col-sm-12">
+        <Card>
+          <Card.Img variant="top" src={photo} />
+          <Card.Body>
+            <div className="d-flex justify-content-between pb-2">
+              <span>{subscribe} students</span>
+              <span>{duration}m</span>
+            </div>
+            <Card.Title className="fw-bolder">{title}</Card.Title>
+            <Card.Text>{titleName}</Card.Text>
+            <Button variant="primary">
+              <Link
+                className="text-decoration-none text-light"
+                to={`/courses/${course.id}`}
+              >
+                Features
+              </Link>
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     );
 };
 
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import Course from '../Course/Course';
+import './Courses.css'
 
 const Courses = () => {
 
@@ -10,16 +11,21 @@ const Courses = () => {
     return (
       <div>
         <h2 className="text-center mb-5">This is Course Page</h2>
-        <Row className="mx-5">
-          <Col lg={3}>
+        <Row className="mx-5 mb-3">
+          <Col lg={2}>
             {courses.map((course) => (
               <p className="mt-4" key={course.id}>
-                <Link to={`/courses/${course.id}`}>{course.title}</Link>
+                <Link
+                  className="text-decoration-none text-dark course-btn"
+                  to={`/courses/${course.id}`}
+                >
+                  {course.title}
+                </Link>
               </p>
             ))}
           </Col>
 
-          <Col lg={9}>
+          <Col lg={10}>
             <div className="row g-3">
               {courses.map((course) => (
                 <Course key={course.id} course={course}></Course>
